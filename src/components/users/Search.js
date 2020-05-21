c1189e8545eb8cc3ec4befa17bae11e3417ff4a9
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 18:12:11 +0100

    added clear functiokn

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
     state = {
         text: ""
-    }
+    };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
+    };
+
 
     onChange = e => this.setState({[e.target.name]: e.target.value});
     
     onSubmit = e => {
         e.preventDefault();
+        this.props.searchUsers(this.state.text)
+        this.setState({text: ""})
     };
 
     render() {
+        const {showClear,clearUsers} = this.props
+
+
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -26,6 +40,15 @@ export class Search extends Component {
                     value="Search" 
                     className="btn btn-dark btn-block" />   
                 </form>
+                {showClear && (
+                 <button 
+            className="btn btn-light btn-block" 
+            onClick={clearUsers}
+            >
+            Clear
+             </button>
+                )}
+           
             </div>
         )
     }
