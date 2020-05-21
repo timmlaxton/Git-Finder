a8627629498fea708cff78428023f85db19290f8
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 17:44:38 +0100

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -14,10 +15,14 @@ class App extends Component {
 
 
   async componentDidMount() {
+    
+    
     this.setState({loading: true});
 
 
-   const res =  await axios.get('https://api.github.com/users');
+   const res = await axios.get(`https://api.github.com/users?client_id=$
+   {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+   {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
    this.setState({users: res.data, loading: false});
   }
@@ -28,6 +33,7 @@ class App extends Component {
       <div className="App">
        <Navbar />
        <div className="container">
+         <Search/>
        <Users loading={this.state.loading} users={this.state.users} />
        </div>
       
