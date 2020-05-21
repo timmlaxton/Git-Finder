52e70c46b6256094018ff226f68955d19cdff38a
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 19:15:08 +0100

    single user passed down

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User'
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About'
@@ -14,8 +15,10 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
+
   }
 
 
@@ -34,6 +37,21 @@ class App extends Component {
     
   }
 
+  // Get single user
+  getUser = async username => {
+    this.setState({loading:true});
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+       process.env.REACT_APP_GITHUB_CLIENT_ID
+     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+     );
+
+     this.setState({user: res.data, loading: false});
+    
+    }
+  
+
   // clear users
 
   clearUsers = () => this.setState({ users: [], loading: false});
@@ -46,7 +64,7 @@ class App extends Component {
   }
 
   render(){
-    const {users, loading} = this.state
+    const {users, loading, user} = this.state
 
 
     return (
@@ -68,6 +86,9 @@ class App extends Component {
              </Fragment>
            )} />
            <Route exact path='/about' component={About} />
+           <Route exact path="/user/:login" render={props => (
+             <User {...props} getUser={this.getUser} user={user} loading={loading} />
+           )}/>
          </Switch>
      
        </div>
