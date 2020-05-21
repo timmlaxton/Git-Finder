5e70bd06f9daaf39b4a51e3693340f4f7e32b904
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 12:12:47 +0100

    imported navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Navbar from './components/layout/Navbar';
 class App extends Component {
   render(){
-    
-const name =  "John Smith";
-const loading = false;
-const showName = true;
-
-
-    return (
+        return (
       <div className="App">
-        {loading ? <h4>Loading....</h4> :  <h1>Hello {showName && name }</h1>}
+        <Navbar />
       
       </div>
     );
