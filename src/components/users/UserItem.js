6e122b55f5998186d086cb9cb97c6bc05c622d9e
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 14:07:17 +0100

    changed clss functions to staless functions

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types';
 
 
-export class UserItem extends Component {
+const UserItem =  ({user: {login, avatar_url, html_url}}) => {
        
-    render() {
-        const {login, avatar_url, html_url} = this.props.user;
+        
 
         return (
             <div className="card text-center">
@@ -18,7 +18,11 @@ export class UserItem extends Component {
               </div>
             </div>
         )
-    }
+    
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem
