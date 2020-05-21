56088d12f4e0b4396f641da76b452bbf311859a3
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 19:43:26 +0100

    added styling

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,18 @@
-import React, {Component} from 'react'
+import React, {Fragment, Component} from 'react'
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom'
 
 export class User extends Component {
 componentDidMount() {
     this.props.getUser(this.props.match.params.login);
 }
 
+static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+}
     render() {
         const {
             name,
@@ -18,15 +26,53 @@ componentDidMount() {
             followings,
             public_repos,
             public_gists,
-            hirable
+            hireable
         } = this.props.user;
 
         const {loading} = this.props;
 
-        return <div>{name}</div>
+        if (loading) return <Spinner />;
+
+        return (
+        <Fragment> 
+            <Link to='/' className='btn btn-light'>
+                Back to search
+                </Link>
+                Hireable: {''}
+                {hireable ? (
+                <i className="fas fa-check text-success" /> 
+                ) : ( <i className="fas fa-times-circle text-danger" /> 
+                )}
+                <div classNamw="card grid-2">
+                <div className="all-center">
+                    <img src={avatar_url} className="round-img" alt="" style={{width: "150px"}}/>
+                    <h1>{name}</h1>
+                    <p>Location: {location} </p>
+                </div>
+                <div>
+                    {bio && (
+                    <Fragment>
+                        <h3>Bio</h3>
+                        <p>{bio}</p>
+                        </Fragment>
+                    )}
+                    <a href={html_url} className="btn btn-dark my-1">
+                        Visit Github profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && <Fragment>
+                                <strong> Username: </strong> {login}
+                               </Fragment>}
+                        </li>
+                    </ul>
+                </div>
+                
+                </div>    
+                 </Fragment> 
             
-        }
+        );
     }
-   
+}
 
 export default User;
