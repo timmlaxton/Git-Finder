27b3e5336f05c464295e43af64f5f81a134f69a0
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 21:21:52 +0100

    added users repos to profile

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
 
   }
 
@@ -51,7 +52,20 @@ class App extends Component {
     
     }
   
+    //Get users repo
 
+ getUserRepos = async username => {
+    this.setState({loading:true});
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+       process.env.REACT_APP_GITHUB_CLIENT_ID
+     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+     );
+
+     this.setState({repos:  res.data, loading: false});
+    
+    }
   // clear users
 
   clearUsers = () => this.setState({ users: [], loading: false});
@@ -64,7 +78,7 @@ class App extends Component {
   }
 
   render(){
-    const {users, loading, user} = this.state
+    const {users, loading, repos, user} = this.state
 
 
     return (
@@ -87,7 +101,13 @@ class App extends Component {
            )} />
            <Route exact path='/about' component={About} />
            <Route exact path="/user/:login" render={props => (
-             <User {...props} getUser={this.getUser} user={user} loading={loading} />
+             <User 
+             {...props} 
+             getUser={this.getUser} 
+             getUserRepos={this.getUserRepos}
+             user={user} 
+             repos={repos}
+             loading={loading} />
            )}/>
          </Switch>
      
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, {Fragment, Component} from 'react'
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import Repos from '../repos/Repos'
 import {Link} from 'react-router-dom'
 
 export class User extends Component {
 componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
 }
 
 static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
 }
     render() {
         const {
@@ -30,7 +34,7 @@ static propTypes = {
             hireable
         } = this.props.user;
 
-        const {loading} = this.props;
+        const {loading, repos} = this.props;
 
         if (loading) return <Spinner />;
 
@@ -87,8 +91,9 @@ static propTypes = {
                 <div className="badge badge-success">Following: {following}</div>
                 <div className="badge badge-light">Public Repos: {public_repos}</div>
                 <div className="badge badge-dark">Public Gists: {public_gists}</div>
-
             </div>
+
+            <Repos repos={repos} />
                  </Fragment> 
             
         ); 
