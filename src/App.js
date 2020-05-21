e9a303fa3275fdd8f18e47e15df83aab35bbfe34
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-21 18:28:23 +0100

    added alert to search bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
+
 import './App.css';
 
 
@@ -10,7 +12,8 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
 
@@ -33,6 +36,12 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false});
 
+  // set Alert
+
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type} });
+    setTimeout(() => this.setState({alert: null}), 2000);
+  }
 
   render(){
     const {users, loading} = this.state
@@ -42,8 +51,12 @@ class App extends Component {
       <div className="App">
        <Navbar />
        <div className="container">
-      <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
-        users.length > 0 ? true : false}
+         <Alert alert={this.state.alert}/>
+      <Search 
+      searchUsers={this.searchUsers} 
+      clearUsers={this.clearUsers} 
+      showClear={users.length > 0 ? true : false}
+      setAlert={this.setAlert}
         />
        <Users loading={loading} users={users} />
        </div>
